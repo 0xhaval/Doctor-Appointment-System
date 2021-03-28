337def4e0f1c3ecd8bed31b92efc112a0c87b16e
0xhaval <oomer5961@gmail.com>
2021-03-28 14:18:00 +0300

    Update database connection

--- config/database.php
+++ config/database.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Str;
 
 return [
 
+    $DATABASE_URL=parse_url('postgres://owbhsldiqnfaiw:91a29defb27b25f1d25fa1b1e5562a63deb248ca5d66abcf905d5f30b1095bff@ec2-54-164-22-242.compute-1.amazonaws.com:5432/d4pi53l3egc00g'),
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
@@ -15,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -46,11 +47,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
